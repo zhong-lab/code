39f5e9b79ae884bba12f8d3ae161628e31246c36
zhong-lab <none>
2019-06-21 14:07:54 -0500

    add attocube code

---
+++ spyre/testing/onthefly.py
@@ -0,0 +1,18 @@
+# Device List
+devices = {
+    # 'fungen':[
+    #     'lantz.drivers.keysight.Keysight_33622A.Keysight_33622A',
+    #     ['USB0::0x0957::0x5707::MY53801461::INSTR'],
+    #     {}
+    # ]
+}
+
+# Experiment List
+spyrelets = {
+    'rabi':[
+        'spyre.spyrelets.onthefly.OnTheFlySpyrelet',
+        {}, 
+        {}
+    ],
+
+}
