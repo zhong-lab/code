99d0c6344ff5cc224b3acb85a0eb897ea73706e7
HP-Desktop <tzhonglab@gmail.com>
2020-10-31 11:49:29 -0500

    autoCommit 2020 1Sa.114928

---
+++ spyre/testing/voltage_scan.py
@@ -0,0 +1,31 @@
+devices = {
+    # 'delaygen':[
+    #     'lantz.drivers.stanford.DG645',
+    #     ['TCPIP0::169.254.29.167::inst0::INSTR'], # connect delay generator by ethernet and use for triggering shutter
+    #     {}
+    # ],
+    # 'fungen':[
+    #     'lantz.drivers.keysight.Keysight_33622A.Keysight_33622A',
+    #     ['TCPIP0::A-33622A-01461.local::inst0::INSTR'], # connecting function generator with ethernet works better for Arb mode
+    #     {}
+    # ],
+    # 'wm':[
+    #     'lantz.drivers.bristol.bristol771.Bristol_771',
+    #     [6535],
+    #     {}
+    # ]
+}
+
+# Experiment List
+spyrelets = {
+    'voltage_scan':[
+        'spyre.spyrelets.voltage_scan_spyrelet.PLThinFilm',
+        {   
+            # 'delaygen':'delaygen'
+            # 'fungen': 'fungen',
+            # 'wm':'wm'
+        }, 
+        {}
+    ],
+
+}
