105a310e88e99c0a183657cc4e86e7787aa240a9
HP-Desktop <tzhonglab@gmail.com>
2020-12-03 11:49:29 -0600

    autoCommit 2020 1Th.114928

--- spyre/spyre/spyrelets/PLpiezo_cwicker_spyrelet.py
+++ spyre/spyre/spyrelets/PLpiezo_cwicker_spyrelet.py
@@ -110,166 +110,6 @@ class PLThinFilm(Spyrelet):
 		self.homelaser(current)
 		wl=self.wm.measure_wavelength()
 		return voltageTargets,totalShift,wl
-
-	"""
-	def record(self,channel):
-		# Set the AWG 
-		self.dataset.clear()
-		self.fungen.clear_mem(1)
-
-		self.fungen.wait()
-
-		params = self.pulse_parameters.widget.get()
-		repeatmag=params['dc repeat unit'].magnitude
-		timestep=params['timestep'].magnitude
-		pulsewidth1=params['pulse width1'].magnitude								
-		pulsewidth1=params['pulse width2'].magnitude
-		freq=params['IFFrequency'].magnitude
-		phase=params['Phase'].magnitude
-		trigperiod = params['period'].magnitude
-		triggerdelay=params['trigger delay'].magnitude
-		awgvoltage=params['Voltage'].magnitude
-		runtime=params['Runtime'].magnitude
-		tau = params['tau'].magnitude
-
-		shutter_offset = params['shutter offset'].magnitude
-
-		# Waves for Spin Echo
-		Wavepi2pulse='Square'   # 'Gaussian' or 'Square'
-		Wavepipulse='Square'
-
-		self.configureQutag()
-		xs=np.array([])
-		ys=np.array([])
-		hist=[]
-		self.dataset.clear()
-		self.fungen.output[1]='OFF'
-		self.fungen.clear_mem(1)
-		self.fungen.wait()
-
-		# Triggering delay
-
-		delay0I = Arbseq_Class_MW('delay0I', timestep,'DC',0,triggerdelay,0,0)
-		repeatwidthdelay0I=(triggerdelay)
-		delay0I.setRepeats(repeatwidthdelay0I)
-		delay0I.create_envelope()
-		delay0I.repeatstring = 'onceWaitTrig'
-
-		# Pi/2 pulse
-
-		pi2PulseI = Arbseq_Class_MW('pi2PulseI', timestep,Wavepi2pulse,Amp_factor_pi2,pulsewidth/2,freq,0)
-		pi2PulseI.delays=predelay
-		pi2PulseI.postdelay=postdelay
-		pi2PulseI.sendTrigger()
-		pi2PulseI.create_envelope()
-
-		# Delay of Tau
-
-		delay1 = Arbseq_Class_MW('delay1', timestep,'DC',0,repeatmag,0,0)
-		repeatwidthdelay1=(tau-1.0*pulsewidth-predelay-postdelay)
-		delay1.setRepeats(repeatwidthdelay1)
-		delay1.create_envelope()
-
-		# Pi Pulse
-
-		piPulseI = Arbseq_Class_MW('piPulseI', timestep,Wavepipulse,0.08,pulsewidth,freq)
-		piPulseI.delays=predelay
-		piPulseI.postdelay=postdelay
-		piPulseI.sendTrigger()
-		piPulseI.create_envelope()
-
-		# Delay of 2 tau
-
-		delay2 = Arbseq_Class_MW('delay2', timestep,'DC',0,repeatmag,0,0) #Block for 2 tau
-		repeatwidthdelay2=(2*tau-pulsewidth-predelay-postdelay)
-		delay2.setRepeats(repeatwidthdelay2)
-		delay2.create_envelope()
-
-		# Send all the Arbs
-
-		self.fungen.send_arb(delay0I, 1)
-		self.fungen.send_arb(pi2PulseI, 1)
-		self.fungen.send_arb(delay1, 1)
-		self.fungen.send_arb(piPulseI, 1)
-		self.fungen.send_arb(delay2, 1)
-
-		# Make sequence
-
-		seq = [delay0I,pi2PulseI, delay1, piPulseI]
-
-		for i in range(0,npulses-1):
-			seq.append(delay2)
-			seq.append(piPulseI)
-
-		self.fungen.create_arbseq('twoPulseI', seq, 1)
-
-		self.fungen.wait()
-		self.fungen.voltage[1] = params['pulse height'].magnitude+0.000000000001*i
-		self.fungen.offset[1] = 0.000
-		print("Voltage is {} , don't remove this line else the AWG will set the voltage to 50 mV".format(self.fungen.voltage[1]))
-
-		time.sleep(1)
-
-		# AWG Output On
-		self.fungen.output[1] = 'ON'
-
-		time.sleep(1)
-
-		# Set the delay generator for triggering the AWG and scope
-
-		self.delaygen.delay['A']=0
-		self.delaygen.delay['B']=10e-9
-		self.delaygen.delay['C']=0
-		self.delaygen.delay['D']=0
-		self.delaygen.Trigger_Source='Internal'
-		self.delaygen.trigger_rate=1/trigperiod
-
-		time.sleep(10)
-
-		# Start collecting data
-		
-		self.fungen.output[1] = 'ON'
-
-		##Qutag Part
-		self.configureQutag()
-		qutagparams = self.qutag_params.widget.get()
-		lost = self.qutag.getLastTimestamps(True) # clear Timestamp buffer
-		stoptimestamp = 0
-		bincount=qutagparams['Bin Count']
-		timebase = self.qutag.getTimebase()
-		start = qutagparams['Start Channel']
-		stop = qutagparams['Stop Channel']
-		stoparray = []
-		self.initHist(bincount)
-
-		startTime = time.time()
-		wls=[]
-		lost = self.qutag.getLastTimestamps(True)
-		looptime=startTime
-		while looptime-startTime < expparams['Measurement Time'].magnitude:
-			loopstart=time.time()
-			# get the lost timestamps
-			lost = self.qutag.getLastTimestamps(True)
-			# wait half a milisecond
-			time.sleep(5*0.1)
-			# get thte timestamps in the last half milisecond
-			timestamps = self.qutag.getLastTimestamps(True)
-
-			tstamp = timestamps[0] # array of timestamps
-			tchannel = timestamps[1] # array of channels
-			values = timestamps[2] # number of recorded timestamps
-
-			for k in range(values):
-				# output all stop events together with the latest start event
-				if tchannel[k] == start:
-					synctimestamp = tstamp[k]
-				else:
-					stoptimestamp = tstamp[k]
-					stoparray.append(stoptimestamp)
-			wl=self.wm.measure_wavelength()
-			wls.append(str(wl))
-			looptime+=time.time()-loopstart
-	"""
 	
 	@Task()
 	def piezo_scan(self,timestep=100e-9):
@@ -552,8 +392,7 @@ class PLThinFilm(Spyrelet):
 		('# of points', {'type': int, 'default': 100}),
 		('Measurement Time', {'type': int, 'default': 300, 'units':'s'}),
 		('File Name', {'type': str}),
-		('AWG Pulse Repetition Period',{'type': float,'default': 0.01,'units':'s'}),
-		('# of Passes', {'type': int, 'default': 100})
+		('AWG Pulse Repetition Period',{'type': float,'default': 0.01,'units':'s'})
 		]
 		w = ParamWidget(params)
 		return w
@@ -564,38 +403,11 @@ class PLThinFilm(Spyrelet):
 	#    ('arbname', {'type': str, 'default': 'arbitrary_name'}),,
 		('Start Channel', {'type': int, 'default': 0}),
 		('Stop Channel', {'type': int, 'default': 2}),
-		('Total Hist Width Multiplier', {'type': int, 'default': 5}),
 		('Bin Count', {'type': int, 'default': 1000})
 		]
 		w = ParamWidget(params)
 		return w
 
-	"""
-	@Element(name='Pulse parameters')
-	# Remove the 'config' file from this location everytime you modify the widget:  'C:\Users\zhong\AppData\Roaming\Spyre\main'
-	def pulse_parameters(self):
-		params = [
-		('period', {'type': float, 'default': 0.1, 'units':'s'}),
-		('shutter offset', {'type': float, 'default': 500e-9, 'units':'s'}),
-		('dc repeat unit', {'type': float, 'default': 50e-9, 'units':'s'}),
-		('trigger delay', {'type': float, 'default': 32e-9, 'units':'s'}),	
-		('timestep', {'type': float, 'default': 1e-9, 'units':'s'}),
-		('period', {'type': float, 'default': 0.1, 'units':'s'}),
-		('nPulses', {'type': int, 'default': 1, 'units':'dimensionless'}),
-		('nAverage', {'type': int, 'default': 50, 'units':'dimensionless'}),
-		('IFFrequency', {'type': float, 'default': 0, 'units':'dimensionless'}),
-		('Phase', {'type': float, 'default': 0, 'units':'dimensionless'}),
-		('pulse width1', {'type': float, 'default': 200e-9, 'units':'s'}),
-		('pulse width2', {'type': float, 'default': 200e-9, 'units':'s'}),
-		('Voltage', {'type': float, 'default': 0.5, 'units':'dimensionless'}),
-		('Runtime', {'type': float, 'default': 1000, 'units':'s'}),				
-		('tau', {'type': float, 'default': 1e-6, 'units':'s'}),
-		('Pi2factor', {'type': float, 'default': 1, 'units':'dimensionless'})
-		]
-		
-		w = ParamWidget(params)
-		return w
-		"""
 	@startpulse.initializer
 	def initialize(self):
 		self.wm.start_data()
--- spyre/testing/PLpiezo_cwicker.py
+++ spyre/testing/PLpiezo_cwicker.py
@@ -1,9 +1,4 @@
 devices = {
-    'delaygen':[
-        'lantz.drivers.stanford.DG645',
-        ['TCPIP0::169.254.29.167::inst0::INSTR'], # connect delay generator by ethernet and use for triggering shutter
-        {}
-    ],
     'fungen':[
         'lantz.drivers.keysight.Keysight_33622A.Keysight_33622A',
         ['TCPIP0::A-33622A-01461.local::inst0::INSTR'], # connecting function generator with ethernet works better for Arb mode
@@ -20,8 +15,7 @@ devices = {
 spyrelets = {
     'PLpiezo_cwicker':[
         'spyre.spyrelets.PLpiezo_cwicker_spyrelet.PLThinFilm',
-        {   
-            'delaygen':'delaygen'
+        {
             'fungen': 'fungen',
             'wm':'wm'
         }, 
