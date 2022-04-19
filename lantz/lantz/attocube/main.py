004d2e6680a6c1a7c0b69f6c2b7f08c35dbf54fa
cwicker <christina.wicker@gmail.com>
2022-04-19 15:16:54 -0500

    Add files via upload

---
+++ lantz/lantz/attocube/main.py
@@ -0,0 +1,10 @@
+from anc350 import ANC350
+
+if __name__ == '__main__':
+    from time import sleep
+    from lantz import Q_
+    from lantz.log import log_to_screen, DEBUG
+
+    log_to_screen(DEBUG)
+
+    inst = ANC350()
